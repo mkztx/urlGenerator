3e90057179c6b54b57521100f958f470b385430a
Maciej <m.rybkowski@icloud.com>
2022-11-17 14:42:00 +0100

    first functionality

---
+++ script.js
@@ -0,0 +1,32 @@
+const inputs = document.querySelector('.inputs');
+const outputs = document.querySelector('.outputs');
+
+let numberOfInputs = 15;
+function createInputs(number) {
+	for (let i = 0; i < numberOfInputs; i++) {
+		const inputBox = document.createElement('div');
+		inputBox.classList.add('Box');
+		const input = document.createElement('input');
+		input.classList.add('input2', `i${i}`);
+		inputBox.appendChild(input);
+		inputs.appendChild(inputBox);
+		const outputBox = document.createElement('div');
+		outputBox.classList.add('Box', 'Text', `${i}`);
+		outputBox.innerText = 'Text';
+		outputs.appendChild(outputBox);
+	}
+}
+
+createInputs(numberOfInputs);
+function output() {
+	const outputBox = document.querySelectorAll('.Text');
+	outputBox.forEach((box) => {
+		const input = document.querySelector(`.i${box.classList[2]}`);
+		let lastText = input.value;
+		const url = document.querySelector('.baseUrl');
+		let baseUrl = url.value;
+		const text = document.querySelector('.firstText');
+		let textBefore = text.value;
+		box.innerText = `${textBefore} ${baseUrl}/${lastText}`;
+	});
+}
