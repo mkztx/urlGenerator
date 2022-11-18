68fc6e226c2f599166d73c764760df3c3e708998
Maciej <m.rybkowski@icloud.com>
2022-11-18 11:07:52 +0100

    added massMode

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const inputs = document.querySelector('.inputs');
 const outputs = document.querySelector('.outputs');
+let mode = '';
 
 let numberOfInputs = 30;
 function createInputs(number) {
-	for (let i = 0; i < numberOfInputs; i++) {
+	for (let i = 0; i < number; i++) {
 		const inputBox = document.createElement('div');
 		inputBox.classList.add('Box');
 		const input = document.createElement('input');
@@ -18,21 +19,70 @@ function createInputs(number) {
 	}
 }
 const arrayOfOutput = [];
-createInputs(numberOfInputs);
+// createInputs(numberOfInputs);
 function output() {
-	const outputBox = document.querySelectorAll('.Text');
-	outputBox.forEach((box) => {
-		const input = document.querySelector(`.i${box.classList[2]}`);
-		let lastText = input.value;
-		const url = document.querySelector('.baseUrl');
-		let baseUrl = url.value;
-		const text = document.querySelector('.firstText');
-		let textBefore = text.value;
-		box.innerText = `${textBefore} ${baseUrl} ${lastText}`;
-		arrayOfOutput.push(`${textBefore} ${baseUrl} ${lastText}`);
-	});
+	if (mode == 'singleMode') {
+		const outputBox = document.querySelectorAll('.Text');
+		outputBox.forEach((box) => {
+			const input = document.querySelector(`.i${box.classList[2]}`);
+			let lastText = input.value;
+			const url = document.querySelector('.baseUrl');
+			let baseUrl = url.value;
+			const text = document.querySelector('.firstText');
+			let textBefore = text.value;
+			box.innerText = `${textBefore} ${baseUrl} ${lastText}`;
+			arrayOfOutput.push(`${textBefore} ${baseUrl} ${lastText}`);
+		});
+	} else if (mode == 'massMode') {
+		//creating output box
+		const outputBox = document.createElement('div');
+		const outputText = document.createElement('p');
+		const massTextValue = document.querySelector('.massInput').value;
+		let output = massTextValue
+			.split('\n')
+			.join(',')
+			.split('.')
+			.join(',')
+			.split(',');
+		for (x in output) {
+			const url = document.querySelector('.baseUrl');
+			let baseUrl = url.value;
+			const text = document.querySelector('.firstText');
+			let textBefore = text.value;
+			let lastText = output[x];
+			arrayOfOutput.push(`${textBefore} ${baseUrl} ${lastText}`);
+		}
+		let convertedOutput = arrayOfOutput.join('\n');
+		outputText.innerText = `${convertedOutput}`;
+		outputBox.appendChild(outputText);
+		outputs.appendChild(outputBox);
+	}
 }
 function copy() {
-	let outputText = arrayOfOutput.join('\n');
-	window.alert(outputText);
+	if (mode == 'singleMode') {
+		let outputText = arrayOfOutput.join('\n');
+		window.alert(outputText);
+	} else if (mode == 'massMode') {
+		let convertedOutput = arrayOfOutput.join('\n');
+		window.alert(convertedOutput);
+	}
+}
+function createSingleMode() {
+	mode = 'singleMode';
+	const number = document.querySelector('.numberOfInputs');
+	createInputs(number.value);
+}
+function massMode() {
+	mode = 'massMode';
+	//creating input box
+	const inputBox = document.createElement('div');
+	const input = document.createElement('textarea');
+	const instruction = document.createElement('p');
+	instruction.innerText = 'Insert all of yours last texts';
+	inputBox.appendChild(instruction);
+	input.classList.add('massInput');
+	input.setAttribute('type', 'text');
+	inputBox.appendChild(input);
+	inputBox.classList.add('massInputBox');
+	inputs.appendChild(inputBox);
 }
